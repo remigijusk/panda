d3f605d861a82e61ae291f55073d3de7490c4d0a
MikasDevtouch <mikas@devtouch.lt>
2025-03-12 15:17:29 +0200

    [ADD] aspa_integration: discounts, returns

--- aspa_integration/__manifest__.py
+++ aspa_integration/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "ASPA integration",
-    "version": "18.0.0.0.4",
+    "version": "18.0.0.0.5",
     "license": "Other proprietary",
     "author": "UAB 'Devtouch!'",
     "website": "https://www.devtouch.lt",
--- aspa_integration/static/src/js/payment_screen_extension.js
+++ aspa_integration/static/src/js/payment_screen_extension.js
@@ -24,20 +24,29 @@ patch(PaymentScreen.prototype, {
 
         try {
             if (order._isRefundOrder()) {
-                const orderLines = order.orderlines.models || order.orderlines;
+                const orderLines = order.lines;
                 let posReference;
                 for (const line of orderLines) {
-                    const originOrder = await this.orm.call("pos.order", "search_read", [
-                        [["lines", "in", line.refunded_orderline_id]],
+                    console.log("line raw id:", line.refunded_orderline_id.raw.id);
+                    const originOrders = await this.orm.call("pos.order", "search_read", [
+                        [["lines", "in", [line.refunded_orderline_id.raw.id]]],
                         ["pos_reference"],
                     ]);
+                    console.log("originOrders:", originOrders);
 
-                    if (originOrder.length > 0) {
-                        posReference = originOrder[0].pos_reference;
+                    if (originOrders && originOrders.length > 0) {
+                        posReference = originOrders[0].pos_reference;
+                        break;
                     } else {
                         console.error("No order found containing the refunded line ID:", line.refunded_orderline_id);
                     }
                 }
+
+                if (!posReference) {
+                    console.error("Previous receipt number is missing for return order.");
+                    throw new Error("Missing previous receipt number for return.");
+                }
+
                 const tillNumber = "1";
                 const receiptNumber = posReference.split("Aspa Receipt:")[1]?.trim();
                 if (!receiptNumber) {
@@ -45,7 +54,7 @@ patch(PaymentScreen.prototype, {
                     throw new Error("Missing previous receipt number for return.");
                 }
 
-                const returnCommand = `G,${tillNumber},${receiptNumber}`;
+                const returnCommand = `G,${tillNumber}-${receiptNumber};`;
                 await aspa.sendCommand("48", returnCommand);
             } else {
                 await aspa.sendCommand("48", "");
@@ -55,19 +64,24 @@ patch(PaymentScreen.prototype, {
         }
     },
 
-    // Register products
+    // register products
     async _registerProducts() {
         const order = this.pos.get_order();
         if (order) {
             const orderLines = order.lines;
             for (const line of orderLines) {
+                if (line.product_id.default_code === "DISC") {
+                    continue;
+                }
+
+                console.log(line);
                 const product = line.product_id;
-                const quantity = line.qty
+                const quantity = line.qty * -1;
                 const taxLetter = product.is_deposit ? "N" : "A";
-                const priceSubtotal = line.get_price_without_tax();
-                const taxAmount = line.tax_ids[0].amount;
-                const price = (priceSubtotal / quantity) * (1 + taxAmount / 100);
-                const parameter = `${product.display_name}\t${taxLetter}${quantity}*${price.toFixed(4)}`;
+                let price = line.getUnitDisplayPriceBeforeDiscount();
+                const discount = line.discount > 0 ? `,-${line.discount}` : "";
+                const parameter = `${product.display_name}\t${taxLetter}${price.toFixed(2)}*${quantity}${discount}`;
+
                 try {
                     await aspa.sendCommand("49", parameter);
                 } catch (error) {
@@ -85,56 +99,75 @@ patch(PaymentScreen.prototype, {
         let receiptNumber;
         const order = this.pos.get_order();
 
-        // Check if there's a card payment and call BankasSale0 before fiscal receipt
-        let cardPaymentSuccess = true;
-        for (const line of paymentLines) {
+        if (!order) {
+            console.error("No order found.");
+            return;
+        }
+
+        let fullAmount;
+        try {
+            const orderLines = Array.isArray(order.lines) ? order.lines : [...order.lines];
+            const discountLine = orderLines.find(line => line.product_id?.default_code === "DISC");
+            let parameterText = "11";
+            if (discountLine) {
+                const discountLineSubtotal = discountLine.price_subtotal_incl * -1;
+                const orderPriceSubtotal = order.get_total_with_tax() + discountLineSubtotal;
+                const discountPercentage = (discountLineSubtotal / orderPriceSubtotal) * 100;
+                parameterText = `11,-${discountPercentage.toFixed()}`;
+            }
+            const subtotalResponse = await aspa.sendCommand("51", parameterText);
+            console.log("subtotalResponse:", subtotalResponse);
+            fullAmount = parseFloat(subtotalResponse.CmdlineResult.split(",")[1]) / 100;
+            console.log("fullAmount:", fullAmount);
+
+        } catch (error) {
+            console.error("Error processing order lines:", error);
+        }
+
+
+        // register single payment line
+        if (paymentLines && paymentLines.length == 1) {
+            console.log('THERE IS ONE PAYMENT LINE');
+            const line = paymentLines[0];
             const paymentType = this._getPaymentType(line.payment_method_id.name);
+            let paymentText = `\t${paymentType}${(Math.floor(fullAmount * 100) / 100).toFixed(2)}`;
+            const roundedFullAmount = (Math.round(fullAmount * 20) / 20).toFixed(2);
+            const paymentAmount = line.amount;
+
             if (paymentType === 'C') {
                 try {
-                    const bankasResponse = await aspa.sendBankas0({ amount: line.amount.toFixed(2) });
-                    console.log("BankasSale0 response:", bankasResponse);
+                    const bankasResponse = await aspa.sendBankas0({ amount: fullAmount });
                     if (!bankasResponse || !bankasResponse.BankasSale0Result.startsWith("OK")) {
                         console.error("Bank card payment failed:", bankasResponse);
-                        cardPaymentSuccess = false;
-                        break;
+                        throw new Error("Bank card payment failed.");
                     }
                 } catch (error) {
                     console.error("Error processing BankasSale0:", error);
-                    cardPaymentSuccess = false;
-                    break;
+                    throw new Error("Bank card payment failed.");
                 }
+            }
+
+            if (paymentAmount == roundedFullAmount && paymentType === 'P') {
+                paymentText = "";
+            }
+
+            try {
+                await aspa.sendCommand("53", paymentText);
+            } catch (error) {
+                console.error("Error registering payment line:", error);
+                throw error;
             }
         }
 
-        if (!cardPaymentSuccess) {
-            await aspa.sendCommand("57", "");
-            throw new Error("Bank card payment unsuccessful. Transaction aborted.");
+        // register multiple payment lines
+        if (paymentLines && paymentLines.length > 1) {
+            await this._processMixedPayments(paymentLines, fullAmount);
         }
 
-        // Register payments
-        if (paymentLines && paymentLines.length > 0) {
-            for (const line of paymentLines) {
-                if (line.amount) {
-                    const paymentType = this._getPaymentType(line.payment_method_id.name);
-                    let paymentText = `\t${paymentType}${line.amount}`;
-                    if (line.amount == order.get_total_with_tax().toFixed(2) && paymentType === 'P') {
-                        paymentText = "";
-                    }
-                    try {
-                        await aspa.sendCommand("53", paymentText);
-                    } catch (error) {
-                        console.error("Error registering payment line:", error);
-                        throw error;
-                    }
-                }
-            }
-        }
-
-        // Finalize fiscal receipt
+        // finalize fiscal receipt
         try {
             const response = await aspa.sendCommand("56", "");
             receiptNumber = response.CmdlineResult.split(",")[1];
-            console.log("Fiscal receipt number:", receiptNumber);
         } catch (error) {
             await aspa.sendCommand("57", "");
             console.error("Error finalizing fiscal receipt:", error);
@@ -142,8 +175,6 @@ patch(PaymentScreen.prototype, {
         }
 
         const finalized = await super._finalizeValidation();
-        console.log("Order", order.raw.id);
-        console.log("Order server ID", order.server_id);
         if (order && order.raw.id) {
             await this.orm.call("pos.order", "update_pos_reference", [
                 [order.raw.id],
@@ -151,6 +182,35 @@ patch(PaymentScreen.prototype, {
             ]);
         }
         return finalized;
+    },
+
+    // process mixed payments
+    async _processMixedPayments(paymentLines, fullAmount) {
+        let remainingAmount = Math.floor(fullAmount * 100) / 100;
+        for (const line of paymentLines) {
+            const paymentType = this._getPaymentType(line.payment_method_id.name);
+            if (paymentType === 'P') {
+                let cashPaymentText = `\t${paymentType}${line.amount.toFixed(2)}`;
+                remainingAmount -= line.amount;
+                try {
+                    await aspa.sendCommand("53", cashPaymentText);
+                } catch (error) {
+                    console.error("Error registering cash payment line:", error);
+                    throw error;
+                }
+
+            } else if (paymentType === 'C') {
+                let cardPaymentText = `\t${paymentType}${remainingAmount.toFixed(2)}`;
+                try {
+                    await aspa.sendBankas0({ amount: remainingAmount });
+                    await aspa.sendCommand("53", cardPaymentText);
+                } catch (error) {
+                    console.error("Error registering card payment line:", error);
+                    throw error;
+                }
+                remainingAmount = 0;
+            }
+        }
     },
 
     _getPaymentType(paymentMethod) {
