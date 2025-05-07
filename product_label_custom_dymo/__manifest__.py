b126aeb93a075d2174f5a2e559719f8d9a92f95b
YuraKrona <yuriy.yakymets@smartteksas.com>
2025-05-07 13:44:38 +0300

    [5777][IMP] product_label_custom_dymo : Custom label with price, unit and country of origin.

---
+++ product_label_custom_dymo/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Custom Product Label (DYMO)",
+    "version": "18.0.0.1.0",
+    "license": "Other proprietary",
+    'summary': 'Custom DYMO product label with country and price incl. TAX',
+    "author": "UAB 'Devtouch!'",
+    "website": "https://www.devtouch.lt",
+    "contributors": [
+        "Yuriy Yakymets <yuriy@devtouch.lt>",
+    ],
+    "category": "All Categories",
+    "depends": [
+        'product'
+    ],
+    "data": [
+        'data/paper_format.xml',
+        'report/report_templates.xml',
+    ],
+    "installable": True,
+    "application": True,
+    "images": [
+        "static/description/icon.png",
+    ],
+}
