08fbde8ed109954d17f6db15d3c50b8f6581c7f4
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-03 19:24:38 +0200

    Add files via upload

---
+++ l10n_lt_kpo_kio_report/l10n_lt_kpo_kio_report/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Lietuvos KPO ir KIO spausdinimas',
+    'version': '18.0.1.1.0',
+    'category': 'Accounting/Localizations',
+    'summary': 'Spausdinti lietuviškus Kasos Pajamų Orderius (KPO) ir Kasos Išlaidų Orderius (KIO)',
+    'description': """
+    Šis modulis prideda galimybę spausdinti Kasos pajamų orderį (KPO) ir Kasos išlaidų orderį (KIO) 
+    tiesiai iš Žurnalo įrašų (account.move). 
+    Automatiškai sugeneruoja sumą žodžiais bei užpildo "Pagrindas" lauką iš operacijos eilučių.
+    """,
+    'author': 'Jūsų Vardas',
+    'website': 'https://www.jusu-domenas.lt',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+        'report/account_move_kpo_kio_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
