164a4982493c6ebb1888af91de6e45aed9e36618
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-03 18:37:59 +0200

    Delete l10n_lt_kpo_kio_report directory

--- l10n_lt_kpo_kio_report/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    x_suma_zodziais = fields.Char(
-        string='Suma žodžiais', 
-        help='Įveskite sumą žodžiais kasos orderiui (pvz.: Šimtas penkiasdešimt eurų 00 ct.)'
-    )
