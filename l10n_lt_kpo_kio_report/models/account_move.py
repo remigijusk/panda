aa51499a9c42f9a200ac0820d1d7283762665ebd
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-03 20:37:34 +0200

    Update account_move.py

--- l10n_lt_kpo_kio_report/models/account_move.py
+++ l10n_lt_kpo_kio_report/models/account_move.py
@@ -8,38 +8,35 @@ class AccountMove(models.Model):
         compute='_compute_kpo_kio_data',
         store=True,
         readonly=False,
-        help='Automatiškai sugeneruota suma žodžiais (galima redaguoti)'
     )
     x_pagrindas = fields.Char(
         string='Pagrindas',
         compute='_compute_kpo_kio_data',
         store=True,
         readonly=False,
-        help='Automatiškai surinkta informacija iš operacijos eilučių'
     )
 
-    @api.depends('line_ids', 'line_ids.debit', 'line_ids.credit', 'line_ids.name', 'currency_id')
+    @api.depends('line_ids', 'line_ids.debit', 'line_ids.credit', 'line_ids.name', 'currency_id', 'partner_id')
     def _compute_kpo_kio_data(self):
         for move in self:
             # Randame kasos sąskaitos eilutę (asset_cash tipo)
             cash_line = move.line_ids.filtered(lambda l: l.account_id.account_type == 'asset_cash')
             
             if cash_line:
-                # Imame pirmą rastą kasos eilutę sumai nustatyti
+                # Imame pirmą rastą kasos eilutę duomenims
                 line = cash_line[0]
                 amount = max(line.debit, line.credit)
                 
-                # Sugeneruojame sumą žodžiais naudojant Odoo standartinį metodą
+                # Sugeneruojame sumą žodžiais pagal partnerio kalbą
                 if move.currency_id:
-                    move.x_suma_zodziais = move.currency_id.amount_to_text(amount)
+                    # Naudojame partnerio kalbą, jei ji nustatyta, kitaip vartotojo kalbą
+                    lang = move.partner_id.lang or self.env.user.lang or 'lt_LT'
+                    move.x_suma_zodziais = move.currency_id.with_context(lang=lang).amount_to_text(amount)
                 else:
                     move.x_suma_zodziais = str(amount)
                 
-                # Surenkame "Pagrindą" iš visų eilučių, kurios NĖRA kasos sąskaita
-                other_lines = move.line_ids.filtered(lambda l: l.account_id.account_type != 'asset_cash' and not l.display_type)
-                labels = [l.name for l in other_lines if l.name]
-                # Pašaliname dublikatus ir sujungiame
-                move.x_pagrindas = ", ".join(list(dict.fromkeys(labels)))
+                # Pagrindas imamas tiesiai iš kasos eilutės etiketės (name)
+                move.x_pagrindas = line.name or ""
             else:
                 move.x_suma_zodziais = ""
                 move.x_pagrindas = ""
