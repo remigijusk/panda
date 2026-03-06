82c0861f1634d3dbe87da59d28cfb43495d75f73
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-06 14:53:58 +0200

    Update __manifest__.py

--- odoo_gemini_receipt/__manifest__.py
+++ odoo_gemini_receipt/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Gemini AI Receipt Scanner (Odoo 18)',
-    'version': '18.0.1.0.17',
+    'version': '18.0.1.0.18',
     'category': 'Accounting',
     'summary': 'Auksinis paketas: AI powered receipt scanning with Vehicle detection & Cash Rounding',
-    'description': 'PILNAS ATNAUJINIMAS: Lietuviškų grynųjų pinigų (Grynieji) ir 0.05 apvalinimo fiksavimas.',
+    'description': 'PILNAS ATNAUJINIMAS: "Up" apvalinimo taisyklės prioritetas ir agresyvus AI detektorius.',
     'author': 'Remigijus',
     'depends': ['account', 'base', 'fleet'],
     'data': [
