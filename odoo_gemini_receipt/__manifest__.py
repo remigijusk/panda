acdc8d5200dabaff9e17896996d7b3ba87387c58
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-06 10:15:44 +0200

    Create __manifest__.py

---
+++ odoo_gemini_receipt/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gemini AI Receipt Scanner (Odoo 18)',
+    'version': '18.0.1.0.2',
+    'category': 'Accounting',
+    'summary': 'AI powered receipt scanning with Vehicle detection',
+    'description': 'Automatinis kvitų nuskaitymas su automobilių atpažinimu pagal ranka rašytą numerį.',
+    'author': 'Remigijus',
+    'depends': ['account', 'base', 'fleet'],
+    'data': [
+        'views/account_move_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
