56062305fe2b73f97d75ac819e18d1fa903604cd
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-11 22:45:22 +0200

    Update hr_employee.py

--- hr_tutas_npd/models/hr_employee.py
+++ hr_tutas_npd/models/hr_employee.py
@@ -14,3 +14,10 @@ class HrEmployee(models.Model):
     ], string='NPD Pasirinkimas', default='netaikyti')
     npd_data = fields.Date(string='NPD Taikymo Data')
     npd_suma = fields.Float(string='Individuali NPD Suma')
+from odoo import models, fields
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    x_is_npd_applied = fields.Boolean(string='Taikyti NPD')
+    x_is_fixed_npd = fields.Boolean(string='Fiksuotas NPD')
