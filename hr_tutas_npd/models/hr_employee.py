2077c31f9e963ff15182757b78868972391cc809
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-11 23:54:06 +0200

    Update hr_employee.py

--- hr_tutas_npd/models/hr_employee.py
+++ hr_tutas_npd/models/hr_employee.py
@@ -3,10 +3,8 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    # Tik šitie du dabar yra pagrindiniai
     x_is_npd_applied = fields.Boolean(string='Taikyti NPD')
     x_is_fixed_npd = fields.Boolean(string='Fiksuotas NPD')
 
-    # Šituos paliekame tik jei jums reikia įrašyti datą ar sumą
     npd_data = fields.Date(string='NPD Taikymo Data')
     npd_suma = fields.Float(string='Individuali NPD Suma')
