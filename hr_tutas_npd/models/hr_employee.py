0890a46bbb9c28b9930fc890d4877e7faf96e4f8
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-11 23:18:07 +0200

    Update hr_employee.py

--- hr_tutas_npd/models/hr_employee.py
+++ hr_tutas_npd/models/hr_employee.py
@@ -3,11 +3,9 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    # Nauji loginiai laukeliai (Checkbox)
     x_is_npd_applied = fields.Boolean(string='Taikyti NPD')
     x_is_fixed_npd = fields.Boolean(string='Fiksuotas NPD')
 
-    # Senieji laukeliai (jei reikia istorijai)
     npd_taikymas = fields.Selection([
         ('taikyti', 'Taikyti NPD'),
         ('netaikyti', 'Netaikyti NPD')
