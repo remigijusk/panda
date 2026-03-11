9fed49e3b97bd4de63196fa6f3aeac60f3c9249d
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-11 23:33:51 +0200

    Update __manifest__.py

--- hr_tutas_npd/__manifest__.py
+++ hr_tutas_npd/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'LT Darbo Apskaita: NPD (Tutas)',
-    'version': '19.0.1.2.3',
+    'version': '19.0.1.2.4',
     'category': 'Human Resources',
     'summary': 'NPD taikymo laukeliai darbuotojo kortelėje',
     'author': 'Remigijus Kubilius',
     'depends': ['hr'],
     'data': [
         'views/hr_employee_views.xml',
-        # 'views/hr_employee_onboarding_templates.xml',  <-- ŠITĄ IŠJUNGIAME
     ],
     'installable': True,
     'application': False,
