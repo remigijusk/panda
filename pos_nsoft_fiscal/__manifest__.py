e165cfb937650f7cd9c793b617ebc481b33c180e
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-28 21:05:25 +0200

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.10.0',
+    'version': '19.0.1.11.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
-    'description': """
-        Modulis skirtas Odoo 19 POS integracijai su nSoft virtualia fiskalizacija.
-        v1.10.0: Sutvarkytas kainų ištraukimas iš Reactive modelių.
-    """,
     'depends': ['point_of_sale'],
     'data': [
         'views/res_config_settings_views.xml',
     ],
     'installable': True,
-    'application': False,
-    'license': 'LGPL-3',
     'assets': {
         'point_of_sale._assets_pos': [
             'pos_nsoft_fiscal/static/src/js/payment_screen.js',
         ],
     },
+    'license': 'LGPL-3',
 }
