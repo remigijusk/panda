9fb6f39a93a0580741af5f32be045cb2ba4cc7b2
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-28 22:00:27 +0200

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.15.0',
+    'version': '19.0.1.16.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
-    'description': """
-        v1.15.0: Pridėtas i.EKA (nSoft) ID spausdinimas ant pirkėjo kvito.
-    """,
     'depends': ['point_of_sale'],
     'data': [
         'views/res_config_settings_views.xml',
@@ -15,7 +12,7 @@
     'assets': {
         'point_of_sale._assets_pos': [
             'pos_nsoft_fiscal/static/src/js/payment_screen.js',
-            'pos_nsoft_fiscal/static/src/xml/OrderReceipt.xml', # Svarbu pridėti čia!
+            'pos_nsoft_fiscal/static/src/xml/OrderReceipt.xml',
         ],
     },
     'license': 'LGPL-3',
