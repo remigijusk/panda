807ce21b2fb1d9e12d70c9a853bff98c28858ccd
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-29 20:20:34 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.33.0',
+    'version': '19.0.1.34.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
     'description': """
-        v1.33.0: Pašalintas lūžtantis vertimų failas. Žodis 'Served by' saugiai keičiamas į 'Aptarnavo' per CSS.
+        v1.34.0: Sutvarkyti besidubliuojantys rekvizitai per Order.prototype. Pridėtas oficialus i18n/lt.po vertimų failas ('Served by' -> 'Aptarnavo').
     """,
     'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
