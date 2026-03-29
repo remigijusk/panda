88c79a982e6719eda7fd6d2e447699f728af7f55
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-29 18:05:55 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.25.0',
+    'version': '19.0.1.26.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
     'description': """
-        v1.25.0: Pridėti privalomi 'origDocNumber' ir 'origCRNumber' laukai ReturnLine schemai, kad veiktų grąžinimai.
+        v1.26.0: Pridėti privalomi 'origDocNumber' ir 'origCRNumber' laukai ReturnLine schemai, kad veiktų grąžinimai.
     """,
+    'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
     'data': [
         'views/res_config_settings_views.xml',
