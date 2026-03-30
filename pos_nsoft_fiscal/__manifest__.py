ceefd1717bc6dd38b274521ce494963e3598c9df
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-30 10:36:36 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.40.0',
+    'version': '19.0.1.43.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
     'description': """
-        v1.40.0: Kietai užkoduoti nSoft prisijungimai, kad 100% veiktų. Sutvarkytas pos.config laukų nuskaitymas.
+        v1.43.0: Pridėtas X Ataskaitos mygtukas POS ekrane ir sutvarkyta Cash In/Out pliuso/minuso logika.
     """,
     'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
@@ -17,6 +17,8 @@
         'point_of_sale._assets_pos': [
             'pos_nsoft_fiscal/static/src/js/payment_screen.js',
             'pos_nsoft_fiscal/static/src/xml/OrderReceipt.xml',
+            'pos_nsoft_fiscal/static/src/js/x_report_button.js',
+            'pos_nsoft_fiscal/static/src/xml/x_report_button.xml',
         ],
     },
     'license': 'LGPL-3',
