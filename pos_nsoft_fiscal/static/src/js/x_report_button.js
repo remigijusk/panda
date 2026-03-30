c8c9233468e331fecbcdacf07500bd2e78f7034a
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-03-30 11:51:27 +0300

    Update x_report_button.js

--- pos_nsoft_fiscal/static/src/js/x_report_button.js
+++ pos_nsoft_fiscal/static/src/js/x_report_button.js
@@ -1,9 +1,8 @@
 /** @odoo-module */
-
 import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { useService } from "@web/core/utils/hooks";
-import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
 
 export class XReportButton extends Component {
     static template = "pos_nsoft_fiscal.XReportButton";
@@ -16,12 +15,17 @@ export class XReportButton extends Component {
 
     async onClick() {
         try {
-            await this.orm.call(
+            this.notification.add("Siunčiama X Ataskaitos komanda...", { type: "info" });
+            const success = await this.orm.call(
                 "pos.session",
                 "print_nsoft_x_report",
                 [[this.pos.pos_session.id]]
             );
-            this.notification.add("X Ataskaita sėkmingai išsiųsta į spausdintuvą!", { type: "success" });
+            if (success) {
+                this.notification.add("X Ataskaita sėkmingai atspausdinta!", { type: "success" });
+            } else {
+                this.notification.add("Nepavyko atspausdinti X Ataskaitos.", { type: "danger" });
+            }
         } catch (error) {
             this.notification.add("Klaida spausdinant X ataskaitą.", { type: "danger" });
             console.error(error);
@@ -29,9 +33,4 @@ export class XReportButton extends Component {
     }
 }
 
-ProductScreen.addControlButton({
-    component: XReportButton,
-    condition: function () {
-        return true;
-    },
-});
+registry.category("pos_control_buttons").add("XReportButton", XReportButton);
