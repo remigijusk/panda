165eed2e5b508139fb61db7ae96725c738d406c1
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-01 21:29:36 +0300

    Create __manifest__.py

---
+++ l10n_lt_kpo_kio/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Lietuvos KPO ir KIO orderiai',
+    'version': '19.0.1.1.0',
+    'category': 'Accounting/Localizations',
+    'summary': 'Pritaikyta Lietuvos rinkai: Kasos pajamų ir išlaidų orderių spausdinimas PDF formatu',
+    'description': """
+        Modulis prideda KPO ir KIO spausdinimo funkcionalumą prie mokėjimų (account.payment).
+        - Suma žodžiu
+        - Privalomi rekvizitai (pagrindas, gavėjas/mokėtojas)
+        - PDF generavimas
+    """,
+    'depends': ['account'],
+    'data': [
+        'report/kpo_kio_report.xml',
+        'report/kpo_kio_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
