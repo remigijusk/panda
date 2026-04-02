b98abad28354b378402a007beb2584efd1b578af
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-02 22:33:26 +0300

    Update __manifest__.py

--- l10n_lt_kpo_kio/__manifest__.py
+++ l10n_lt_kpo_kio/__manifest__.py
@@ -1,14 +1,16 @@
 {
     'name': 'Lietuvos KPO ir KIO orderiai (Suderinimo langui)',
-    'version': '19.0.4.0.0',
+    'version': '19.0.5.0.0',
     'author': 'Remigijus Kubilius',
     'category': 'Accounting/Localizations',
-    'summary': 'KPO/KIO spausdinimas su tiksliais rekvizitais ir dinamišku kasininku',
+    'summary': 'KPO/KIO spausdinimas su tiksliais rekvizitais ir pritaikytu A4 formatu',
     'description': """
         Modulis prideda klasikinės formos KPO/KIO spausdinimą.
+        - Veikia tiesiogiai iš "Banko sąskaitos suderinimas" lango.
         - Dinamiškas kasininko vardo/pavardės paėmimas.
         - Teisingas įmonės kodo atvaizdavimas.
         - Mokėtojo/Gavėjo parašų laukai.
+        - Priskirtas specialus A4 popieriaus formatas su sumažintomis paraštėmis (telpa į vieną lapą).
     """,
     'depends': ['account'],
     'data': [
