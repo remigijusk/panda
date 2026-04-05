521c25ce0c95ec726e7cdbf25a82ffedb0a72bbc
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-05 12:01:33 +0300

    Update __manifest__.py

--- l10n_lt_kpo_kio/__manifest__.py
+++ l10n_lt_kpo_kio/__manifest__.py
@@ -1,23 +1,22 @@
 {
-    'name': 'Lietuvos KPO ir KIO orderiai (Suderinimo langui)',
-    'version': '19.0.5.0.0',
+    'name': 'Lietuvos KPO ir KIO orderiai (Suderinimo langui + POS)',
+    'version': '19.0.10.0.0',
     'author': 'Remigijus Kubilius',
     'category': 'Accounting/Localizations',
-    'summary': 'KPO/KIO spausdinimas su tiksliais rekvizitais ir pritaikytu A4 formatu',
-    'description': """
-        Modulis prideda klasikinės formos KPO/KIO spausdinimą.
-        - Veikia tiesiogiai iš "Banko sąskaitos suderinimas" lango.
-        - Dinamiškas kasininko vardo/pavardės paėmimas.
-        - Teisingas įmonės kodo atvaizdavimas.
-        - Mokėtojo/Gavėjo parašų laukai.
-        - Priskirtas specialus A4 popieriaus formatas su sumažintomis paraštėmis (telpa į vieną lapą).
-    """,
-    'depends': ['account'],
+    'summary': 'KPO/KIO spausdinimas iš banko suderinimo ir tiesiai iš POS',
+    'depends': ['account', 'sign', 'point_of_sale'],
     'data': [
         'report/kpo_kio_report.xml',
         'report/kpo_kio_template.xml',
+        'report/pos_kpo_report.xml',
+        'views/res_config_settings_views.xml',
     ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'l10n_lt_kpo_kio/static/src/js/pos_kpo_button.js',
+            'l10n_lt_kpo_kio/static/src/xml/pos_kpo_button.xml',
+        ],
+    },
     'installable': True,
-    'application': False,
     'license': 'LGPL-3',
 }
