054d4ca7dee19113ed4418907ceb233d12158a2a
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-05 12:57:59 +0300

    Update res_config_settings.py

--- pos_nsoft_fiscal/models/res_config_settings.py
+++ pos_nsoft_fiscal/models/res_config_settings.py
@@ -4,6 +4,8 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    pos_nsoft_api_url = fields.Char(string='nSoft API Base URL', config_parameter='pos_nsoft_fiscal.api_url')
-    pos_nsoft_pos_id = fields.Char(string='nSoft Kasos ID', config_parameter='pos_nsoft_fiscal.pos_id')
-    pos_nsoft_api_token = fields.Char(string='nSoft API Token', config_parameter='pos_nsoft_fiscal.api_token')
+    # Ryšys su konkrečia kasa (pos.config)
+    pos_nsoft_enabled = fields.Boolean(related='pos_config_id.nsoft_enabled', readonly=False)
+    pos_nsoft_api_url = fields.Char(related='pos_config_id.nsoft_api_url', readonly=False)
+    pos_nsoft_pos_id = fields.Char(related='pos_config_id.nsoft_pos_id', readonly=False)
+    pos_nsoft_token = fields.Char(related='pos_config_id.nsoft_token', readonly=False)
