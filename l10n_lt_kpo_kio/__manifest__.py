18c5a4be0c608dda1fc3cc4c101249dc3132b69c
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-06 14:26:47 +0300

    Update __manifest__.py

--- l10n_lt_kpo_kio/__manifest__.py
+++ l10n_lt_kpo_kio/__manifest__.py
@@ -1,17 +1,25 @@
 {
-    'name': 'Lietuvos KPO ir KIO orderiai (Suderinimo langui + POS)',
-    'version': '19.0.27.0.0',
+    'name': 'Lietuvos KPO POS Orderis (Galutinis variantas)',
+    'version': '19.0.28.0.0',
     'author': 'Remigijus Kubilius',
-    'category': 'Accounting/Localizations',
-    'summary': 'KPO/KIO spausdinimas su automatiniais parašais ir POS integracija',
+    'category': 'Point of Sale/Localizations',
+    'summary': 'Standartinės formos KPO spausdinimas su automatiniais parašais tiesiai iš POS užsakymo lango',
+    'description': """
+        Modulis prideda klasikinės formos KPO spausdinimą iš Pardavimo taško.
+        - Mygtukas atsira POS užsakymo lange (Header).
+        - Mygtukas matomas tik jei apmokėta per KPO ir jei įjungtas nustatymas.
+        - Pilnas A4 PDF šablonas su orderiu (viršuje) ir kvitu (apačioje).
+        - Pilni rekvizitai, serijos, numeriai ir pagrindai abiejose dalyse.
+        - AUTOMATINIS KASININKO PARAŠAS: Paima vartotojo skaitmeninį parašą iš Odoo profilio.
+    """,
     'depends': ['account', 'sign', 'point_of_sale'],
     'data': [
-        'report/kpo_kio_report.xml',
-        'report/kpo_kio_template.xml',
-        'report/pos_kpo_report.xml',
+        'report/kpo_report.xml',
+        'report/kpo_report_template.xml',
         'views/res_config_settings_views.xml',
-        'views/pos_order_views.xml', # Naujas failas mygtukui
+        'views/pos_order_views.xml',
     ],
     'installable': True,
+    'application': False,
     'license': 'LGPL-3',
 }
