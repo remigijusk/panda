5b8b2d6d7c6c9f69b4fbe4ff8f8b0d7717657631
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-06 15:26:16 +0300

    Update __manifest__.py

--- l10n_lt_kpo_kio/__manifest__.py
+++ l10n_lt_kpo_kio/__manifest__.py
@@ -1,14 +1,14 @@
 {
     'name': 'Lietuvos KPO ir KIO orderiai (Kasos žurnalui + POS)',
-    'version': '19.0.31.0.0',
+    'version': '19.0.32.0.0',
     'author': 'Remigijus Kubilius',
     'category': 'Accounting/Localizations',
     'summary': 'KPO/KIO spausdinimas iš banko sudengimų ir POS užsakymų',
     'depends': ['account', 'sign', 'point_of_sale'],
     'data': [
+        'report/kpo_kio_reports.xml',
         'views/res_config_settings_views.xml',
         'views/pos_order_views.xml',
-        'report/kpo_kio_reports.xml',
     ],
     'installable': True,
     'license': 'LGPL-3',
