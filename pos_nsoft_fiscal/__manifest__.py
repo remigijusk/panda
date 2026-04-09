b383deb72683a3608eaaec2ecd809b310dfbedfe
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-09 21:42:55 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.60.0',
+    'version': '19.0.1.61.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
     'description': """
-        v1.60.0: Ištaisyta JS patch() sintaksės klaida Odoo 19 aplinkoje (palikti 2 argumentai). Pridėtas saugus kintamųjų tikrinimas XML faile, užkertant kelią balto ekrano OWL klaidoms.
+        v1.61.0: Galutinai išspręsta "Error loading javascript modules" problema. Pašalintas pavojingas Order modelio importavimas (kurio kelias Odoo 19 versijoje pasikeitė). Įdiegtas saugus "monkey-patching" metodas čekio duomenims papildyti tiesiogiai iš PaymentScreen.
     """,
     'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
