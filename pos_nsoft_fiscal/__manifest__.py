6507f8e0959f604a2ba01ab5ddbd08dd8dbd043a
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-09 23:44:09 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.67.0',
+    'version': '19.0.1.68.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
-    'description': """
-        v1.67.0: Pakeista "Testuoti ryšį" mygtuko logika. Užuot siuntus 0.00 EUR pinigų įnešimą (kurį nSoft atmeta su 400 Bad Request klaida), dabar ryšys testuojamas iškviečiant X ataskaitą (/cur-day).
-    """,
     'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
     'data': [
@@ -14,5 +11,11 @@
         'views/pos_session_views.xml',
     ],
     'installable': True,
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_nsoft_fiscal/static/src/js/payment_screen.js',
+            'pos_nsoft_fiscal/static/src/xml/OrderReceipt.xml',
+        ],
+    },
     'license': 'LGPL-3',
 }
