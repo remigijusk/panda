f3786cb217ea6a4177d173e6877728aaf40bfc4f
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-09 22:44:25 +0300

    Update __manifest__.py

--- pos_nsoft_fiscal/__manifest__.py
+++ pos_nsoft_fiscal/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'nSoft Virtual Fiscalization for POS',
-    'version': '19.0.1.65.0',
+    'version': '19.0.1.66.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Tiesioginė nVirtualFiscal (i.EKA) integracija per REST API',
     'description': """
-        v1.65.0: Atsisakyta nepatikimo ir klaidų sukeliančio JS 'patching' metodo PaymentScreen lange. Logika perkelta išskirtinai į Python backend (pos_order.py) ir XML šabloną. Atstatytas 100% Odoo stabilumas, išsaugant pilną nSoft funkcionalumą pajungtose kasose.
+        v1.66.0: Pilnai ištaisyta OWL Lifecycle balto ekrano klaida. Pašalintos nesaugios XML užklausos (props.data.pos_order). Dabar nepajungtos kasos veikia 100% standartiškai, neieško nSoft duomenų ir nelūžta.
     """,
     'author': 'Remigijus Kubilius',
     'depends': ['point_of_sale'],
