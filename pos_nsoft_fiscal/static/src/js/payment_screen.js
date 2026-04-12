c00b14f67a2a84633dcd922a04d09c4c7a44a152
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-12 13:44:41 +0300

    Update payment_screen.js

--- pos_nsoft_fiscal/static/src/js/payment_screen.js
+++ pos_nsoft_fiscal/static/src/js/payment_screen.js
@@ -1,33 +1,21 @@
 /** @odoo-module */
 /**
- * nSoft fiscalization - frontend patch (Odoo 19 OWL)
+ * nSoft fiscalization - frontend patch (Odoo 19)
  *
- * Backend saves nsoft_receipt_id to pos.order via _process_order.
- * _loader_params_pos_order loads this field into POS client.
- *
- * In Odoo 19, OrderReceipt template receives props.data from ReceiptScreen.
- * We patch ReceiptScreen to inject nsoft fields into the receipt data.
+ * Extends PosOrder model to include nsoft fields in export_for_printing.
+ * Backend saves nsoft_receipt_id via _process_order.
+ * _loader_params_pos_order loads the field into POS client.
  */
 
+import { PosOrder } from "@point_of_sale/app/models/pos_order";
 import { patch } from "@web/core/utils/patch";
-import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
 
-patch(ReceiptScreen.prototype, {
-    get nextScreen() {
-        return super.nextScreen;
-    },
-
-    get receiptData() {
-        const data = super.receiptData;
-        try {
-            const order = this.currentOrder;
-            if (order && data) {
-                data.nsoft_receipt_id = order.nsoft_receipt_id || false;
-                data.nsoft_error = order.nsoft_error || false;
-            }
-        } catch (e) {
-            // Silent fail - do not break receipt if patching fails
-        }
-        return data;
+patch(PosOrder.prototype, {
+    //@override
+    export_for_printing(baseUrl, headerData) {
+        const result = super.export_for_printing(baseUrl, headerData);
+        result.nsoft_receipt_id = this.nsoft_receipt_id || false;
+        result.nsoft_error = this.nsoft_error || false;
+        return result;
     },
 });
