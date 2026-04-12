cedd2c8887174a8c3110b6e6278d6b6c513cd5a1
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-12 13:31:52 +0300

    Update payment_screen.js

--- pos_nsoft_fiscal/static/src/js/payment_screen.js
+++ pos_nsoft_fiscal/static/src/js/payment_screen.js
@@ -2,26 +2,31 @@
 /**
  * nSoft fiscalization - frontend patch (Odoo 19 OWL)
  *
- * The backend already saves nsoft_receipt_id to pos.order via _process_order.
- * _loader_params_pos_order loads this field into the POS client.
- * This patch injects nsoft fields into the receipt props so the template can render them.
+ * Backend saves nsoft_receipt_id to pos.order via _process_order.
+ * _loader_params_pos_order loads this field into POS client.
+ *
+ * In Odoo 19, OrderReceipt template receives props.data from ReceiptScreen.
+ * We patch ReceiptScreen to inject nsoft fields into the receipt data.
  */
 
 import { patch } from "@web/core/utils/patch";
 import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
 
 patch(ReceiptScreen.prototype, {
-    get orderUiState() {
-        const state = super.orderUiState;
-        return state;
+    get nextScreen() {
+        return super.nextScreen;
     },
 
     get receiptData() {
-        const data = super.receiptData || {};
-        const order = this.currentOrder;
-        if (order) {
-            data.nsoft_receipt_id = order.nsoft_receipt_id || false;
-            data.nsoft_error = order.nsoft_error || false;
+        const data = super.receiptData;
+        try {
+            const order = this.currentOrder;
+            if (order && data) {
+                data.nsoft_receipt_id = order.nsoft_receipt_id || false;
+                data.nsoft_error = order.nsoft_error || false;
+            }
+        } catch (e) {
+            // Silent fail - do not break receipt if patching fails
         }
         return data;
     },
