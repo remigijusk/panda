60b2f0a071fe832eea82b3f239e15d7293bcbd7f
remigijusk <81972207+remigijusk@users.noreply.github.com>
2026-04-13 21:27:03 +0300

    Update pos_config.py

--- pos_nsoft_fiscal/models/pos_config.py
+++ pos_nsoft_fiscal/models/pos_config.py
@@ -7,6 +7,9 @@ class PosConfig(models.Model):
     nsoft_enabled = fields.Boolean(string="Naudoti nSoft fiskalizaciją", default=False)
     nsoft_api_url = fields.Char(string="nSoft API URL", help="Pvz.: https://nvf.app3.nsoft.eu:30032")
     nsoft_pos_id = fields.Char(string="nSoft POS ID", help="Pvz.: CR-000019280")
+    nsoft_vat_group_21 = fields.Char(string="PVM grupė 21%", default='A', help="nSoft PVM grupės pavadinimas 21% tarifui (pvz.: A)")
+    nsoft_vat_group_9 = fields.Char(string="PVM grupė 9%", default='E', help="nSoft PVM grupės pavadinimas 9% tarifui (pvz.: E)")
+    nsoft_vat_group_0 = fields.Char(string="PVM grupė 0%", default='F', help="nSoft PVM grupės pavadinimas 0% tarifui (pvz.: F)")
     nsoft_token = fields.Char(string="nSoft Token")
     # nSoft PVM grupių pavadinimai (priklauso nuo kasėjo konfigūracijos)
     nsoft_vat_group_21 = fields.Char(
